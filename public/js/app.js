c84cbac28695ee9545d5ba5c622fe9de0e54c01e
kassirikm <kassirikm@gmail.com>
2020-07-20 14:24:46 +0200

    test

--- public/js/app.js
+++ public/js/app.js
@@ -49674,8 +49674,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\laragon\www\theater\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\laragon\www\theater\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\laragon\www\tfebot\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\laragon\www\tfebot\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
